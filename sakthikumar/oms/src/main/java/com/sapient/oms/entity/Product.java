8fa74db0435de5a516dcee1e402e37451e6bfbca
sakthi2kx <sakthikumar2kx@gmail.com>
2022-08-04 13:56:39 +0530

    first commit

---
+++ sakthikumar/oms/src/main/java/com/sapient/oms/entity/Product.java
@@ -0,0 +1,12 @@
+package com.sapient.oms.entity;
+
+import java.util.Date;
+
+public class Product {
+    private String name;
+    private String id;
+    static int count;
+    private int price;
+    private Date manufactureDate;
+    private Date expiryDate;
+}
