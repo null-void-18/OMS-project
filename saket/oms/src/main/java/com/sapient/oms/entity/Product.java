9197d2b4417d2f5ac65efcbae712b6b57146d8c5
saket-1 <saket06205@gmail.com>
2022-08-04 13:40:15 +0530

    Adding oms folder

---
+++ saket/oms/src/main/java/com/sapient/oms/entity/Product.java
@@ -0,0 +1,10 @@
+package com.sapient.oms.entity;
+
+import java.time.LocalTime;
+
+public class Product {
+    private int id;
+    private String name;
+    private float price;
+    private float ratings;
+}
