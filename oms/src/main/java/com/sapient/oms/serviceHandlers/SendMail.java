fd9b8d2906a588220d1d857463a1a3a2560e3c92
KiranSekhar <kirankoolkidoz@gmail.com>
2022-08-12 17:21:14 +0530

    added email code PJUA-218

---
+++ oms/src/main/java/com/sapient/oms/serviceHandlers/SendMail.java
@@ -0,0 +1,56 @@
+package com.sapient.oms.serviceHandlers;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.Session;
+import javax.mail.Transport;
+ 
+ 
+public class SendMail
+{
+ 
+   public static void main(String [] args)
+   {   
+      String recipient = "kirankoolkidoz@gmail.com";
+ 
+      String sender = "kiransekhar18@gmail.com";
+ 
+      String host = "smtp.gmail.com";
+ 
+      Properties properties = System.getProperties();
+ 
+      properties.put("mail.smtp.host", host);
+      properties.put("mail.smtp.port", "465");
+      properties.put("mail.smtp.ssl.enable", "true");
+      properties.put("mail.smtp.auth", "true");
+
+ 
+      Session session = Session.getInstance(properties,new Authenticator() {
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication(){
+            return new PasswordAuthentication("kiransekhar18@gmail.com", "kiranceg2001");
+        }
+      });
+ 
+      try
+      {
+         MimeMessage message = new MimeMessage(session);
+ 
+         message.setFrom(sender);
+ 
+         message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+ 
+         message.setSubject("This is Subject");
+ 
+         message.setText("This is a test mail");
+ 
+         Transport.send(message);
+         System.out.println("Mail successfully sent");
+      }
+      catch (MessagingException mex)
+      {
+         mex.printStackTrace();
+      }
+   }
+}
